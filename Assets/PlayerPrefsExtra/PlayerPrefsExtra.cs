5545c09b3cbdd1170a806203bb91c8e9a60f717e
hamza herbou <hamza95herbou@gmail.com>
2021-04-22 14:46:07 +0000

    Update PlayerPrefsExtra.cs

--- Assets/PlayerPrefsExtra/PlayerPrefsExtra.cs
+++ Assets/PlayerPrefsExtra/PlayerPrefsExtra.cs
@@ -18,12 +18,12 @@ public static class PlayerPrefsExtra
 
 	public static bool GetBool (string key)
 	{
-		return (PlayerPrefs.GetInt (key, 0) == 1) ? true : false;
+		return (PlayerPrefs.GetInt (key, 0) == 1);
 	}
 
 	public static bool GetBool (string key, bool defaultValue)
 	{
-		return (PlayerPrefs.GetInt (key, (defaultValue ? 1 : 0)) == 1) ? true : false;
+		return (PlayerPrefs.GetInt (key, (defaultValue ? 1 : 0)) == 1);
 	}
 
 	public static void SetBool (string key, bool value)
